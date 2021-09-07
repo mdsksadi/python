a69d1eea99e8474d6fa8533247a25631b36440e5
Shekh Sadi <sadiewu12345@gmail.com>
2021-09-07 02:11:20 +0200

    002 Getting Started With Python

---
+++ Getting_Started_With_Python.py
@@ -0,0 +1,24 @@
+
+print(2+3)              #Print is a function by which we can show something. When we use "()", it means it's a function.
+print(3-2)
+print(2*3)
+print(6/2)              #Although both of the values are integer, but the relust is float
+print(6//2)             #"//" shows only integer values
+print(5/2)              #It shows fraction value
+print(5//2)             #Now shows only integer value
+print(5%2)              #"%" shows the reminder
+
+print('Sadi')               #Single and double quatation is same
+print('Sadi\'s book')       #If we use "\'", copiler ingore the quatatuon
+print("Sadi's book")
+
+print(10)
+print(10,"Euro", 50," Cents")   #For number, no need to put quatationa and need to put comma "," instead of plus "+"
+
+print("Sadi's" + ' "book"') #First we need to thing, then use the single or dubble quatation
+
+print("Sadi"+"Sadi")        #By writing a "+" sign we can connect a string
+print(10*"Sadi")            #It shows 10 times Sadi
+
+print("D:\Python\Telusko\new\line") #"\n" means new line
+print(r"D:\Python\Telusko")         #If I put "r" befor the string, it means raw string
